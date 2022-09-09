12d059a824d55615291503dc89f2584665b751d5
Johan Thomsen <jth@dbc.dk>
2022-09-09 11:28:16 +0200

    fixed shadow setup

---
+++ docker.nix
@@ -0,0 +1,48 @@
+{ pkgs ? (import <nixpkgs> {}) }: with pkgs; with pkgs.lib;
+let
+  savedir = "/data/savegames";
+
+  game = writeScript "game.sh" ''
+    #!${runtimeShell}
+    cd ${savedir}
+    ${frotz}/bin/frotz ${./ZORK1.DAT}
+  '';
+
+  entry = writeScript "entry.sh" ''
+    #!${runtimeShell}
+    set -e
+
+    mkdir -p ${savedir}
+    chown zork ${savedir}
+    chmod 755 ${savedir}
+
+    if [ ! -f /data/ssh_host_rsa_key ]; then
+      ssh-keygen -f /data/ssh_host_rsa_key -N "" -t rsa
+    fi
+    if [ ! -f /data/ssh_host_dsa_key ]; then
+      ssh-keygen -f /data/ssh_host_dsa_key -N "" -t dsa
+    fi
+
+    stop() {
+      echo "Received SIGINT or SIGTERM. Shutting down"
+      PID=$(cat /run/sshd.pid)
+      wait $PID
+    }
+
+    echo "Starting sshd"
+    mkdir /run
+    trap stop SIGINT SIGTERM
+    ${openssh}/bin/sshd -eDf "${./sshd_config}"
+  '';
+in
+{
+
+  contents = with pkgs; [ coreutils openssh (import ./shadow.nix { inherit pkgs game; })];
+
+  config = {
+    Entrypoint = [ entry ];
+    ExposedPorts = {
+     "22/tcp" = {};
+    };
+  };
+}
---
+++ shadow.nix
@@ -0,0 +1,37 @@
+{ pkgs ? (import <nixpkgs> {}), game }:
+pkgs.buildEnv {
+    name = "shadow-setup";
+    postBuild = ''
+      mkdir -p $out/var/empty $out/home/zork
+    '';
+    paths = [
+      (
+      pkgs.writeTextDir "etc/shadow" ''
+        root:!x:::::::
+        sshd:!x:::::::
+        zork:$6$mXvH05umiRGrM.Fe$hw8XDJuQ.7HkBRiBxuJGg5EoK8mT37vo.vWfkm2HzbPki2LfSbSJU1DaEkUwBD2BV/kIGKRvpacSEPVi3yZ2q.:::::::
+      ''
+      )
+      (
+      pkgs.writeTextDir "etc/passwd" ''
+        root:x:0:0::/root:/dev/null
+        sshd:x:74:74::/var/empty:/dev/null
+        zork:x:1000:1000::/home/zork:${game}
+      ''
+      )
+      (
+      pkgs.writeTextDir "etc/group" ''
+        root:x:0:
+        sshd:x:74:
+        zork:x:1000:
+      ''
+      )
+      (
+      pkgs.writeTextDir "etc/gshadow" ''
+        root:x::
+        sshd:x::
+        zork:x::
+      ''
+      )
+    ];
+  }
